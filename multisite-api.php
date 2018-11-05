c54c1bd62ddf4e51151c6b5615c517e12dd4f8a4
Andrew Zito <zitoa@lafayette.edu>
2018-11-05 10:05:39 -0500

    Move functions into controller class

--- multisite-api.php
+++ multisite-api.php
@@ -10,90 +10,93 @@
  * @package         multisite-api
  */
 
-/**
- * Send a text message.
- *
- * @param WP_REST_Request $request The request object.
- * @return boolean
- */
-function multisite_api_list_sites( WP_REST_Request $request ) {
-	// Get plugin options.
-	// $options = get_option( 'multisite_api_settings' );
-
-	// Do nothing if we're missing the catalog URL.
-	// if ( ! $options['multisite_api_url'] ) {
-	// 	return false;
-	// }
-
-	echo json_encode(get_sites());
-	exit;
-}
-
-function multisite_api_create_site( WP_REST_Request $request ) {
-
-	$params = $request->get_params();
-
-	$site = get_current_site();
-	$domain = $site->domain;
-	$path = '/' . $params['name'];
-	$title = $params['title'];
-    $admin = $params['admin'];
-
-    if (!is_numeric($admin)) {
-        $admin = get_user_by('login', $params['admin']);
-    }
-
-	echo "Attempting to create blog with:\n";
-	echo "  Domain: $domain\n";
-	echo "  Path: $path\n";
-	echo "  Title: $title\n";
-	echo "  Admin ID: $user_id\n";
-
-	$result = wpmu_create_blog( $domain, $path, $title, $user_id );
-
-	if (is_numeric($result)) {
-		echo "Site created! ID: $result";
-	} else {
-		echo "Sorry, site could not be created because...\n";
-		echo json_encode($result);
+class Multisite_API_Controller {
+	public function __construct() {
+		$this->namespace = '/multisite/v2';
+		$this->register_routes();
 	}
-	exit;
-}
 
-$namespace = 'multisite/v2';
-
-add_action( 'rest_api_init', function() {
-	register_rest_route( $namespace, '/list/', array(
-		'methods' => 'POST',
-		'callback' => 'multisite_api_list_sites',
-    ) );
-    register_rest_route( $namespace, '/delete/', array(
-		'methods' => 'POST',
-		'callback' => 'multisite_api_delete_site',
-		'args' => array(
-            'id' => array(
-                'default' => false,
-            ),
-            'path' => array(
-                'default' => false,
-            )
-        )
-    ) );
-	register_rest_route( $namespace, '/create/', array(
-		'methods' => 'POST',
-		'callback' => 'multisite_api_create_site',
-		'args' => array(
-			'name' => array(
-				'default' => false,
+	public function register_routes() {
+		register_rest_route( $this->namespace, '/list/', array(
+			'methods' => 'POST',
+			'callback' => 'multisite_api_list_sites',
+		) );
+		register_rest_route( $this->namespace, '/delete/', array(
+			'methods' => 'POST',
+			'callback' => 'multisite_api_delete_site',
+			'args' => array(
+				'id' => array(
+					'default' => false,
+				),
+				'path' => array(
+					'default' => false,
+				)
+			)
+		) );
+		register_rest_route( $this->namespace, '/create/', array(
+			'methods' => 'POST',
+			'callback' => 'multisite_api_create_site',
+			'args' => array(
+				'name' => array(
+					'default' => false,
+				),
+				'title' => array(
+					'default' => false,
+				),
+				'admin' => array(
+					'default' => 1,
+				)
 			),
-			'title' => array(
-				'default' => false,
-            ),
-            'admin' => array(
-                'default' => 1,
-            )
-		),
-    ) );
+		) );
+	}
+
+	public function multisite_api_list_sites( WP_REST_Request $request ) {
+		// Get plugin options.
+		// $options = get_option( 'multisite_api_settings' );
+
+		// Do nothing if we're missing the catalog URL.
+		// if ( ! $options['multisite_api_url'] ) {
+		// 	return false;
+		// }
+
+		echo json_encode(get_sites());
+		exit;
+	}
+
+	public function multisite_api_create_site( WP_REST_Request $request ) {
+
+		$params = $request->get_params();
+
+		$site = get_current_site();
+		$domain = $site->domain;
+		$path = '/' . $params['name'];
+		$title = $params['title'];
+		$admin = $params['admin'];
+
+		if (!is_numeric($admin)) {
+			$admin = get_user_by('login', $params['admin']);
+		}
+
+		echo "Attempting to create blog with:\n";
+		echo "  Domain: $domain\n";
+		echo "  Path: $path\n";
+		echo "  Title: $title\n";
+		echo "  Admin ID: $user_id\n";
+
+		$result = wpmu_create_blog( $domain, $path, $title, $user_id );
+
+		if (is_numeric($result)) {
+			echo "Site created! ID: $result";
+		} else {
+			echo "Sorry, site could not be created because...\n";
+			echo json_encode($result);
+		}
+		exit;
+	}
+
+}
+add_action( 'rest_api_init', function() {
+	new Multisite_API_Controller();
 } );
 
 // if ( is_admin() ) {
